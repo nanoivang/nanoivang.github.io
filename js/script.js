0807bfc79c666e0a461649b30737e83da22b8605
nanoi <nanoivang@gmail.com>
2020-02-08 05:48:27 +0900

    asd

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function test() {
+    var a = prompt("?")
+    something(a)
+}
+
+function something(str) {
+    if (str == "asd") {
+        alert("El. Psy. Kongroo")
+    }
+    else {
+        alert("ok")
+    }
+}
